21cdcdc83311c35668afeab3e7eef58b51ef0ec9
BAGUIAN HAROUNA <baguianharouna@Hbag-MBP.lan>
2023-10-18 01:25:45 +0200

    début_fusion_signup

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body"),
-      modeToggle = body.querySelector(".mode-toggle");
-      sidebar = body.querySelector("nav");
-      sidebarToggle = body.querySelector(".sidebar-toggle");
+modeToggle = body.querySelector(".mode-toggle");
+sidebar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
 
 let getMode = localStorage.getItem("mode");
 if(getMode && getMode ==="dark"){
@@ -30,3 +30,67 @@ sidebarToggle.addEventListener("click", () => {
         localStorage.setItem("status", "open");
     }
 })
+
+
+
+
+function chargerContenu() {
+ 
+    fetch('signup.html')
+        .then(response => response.text())
+        .then(data => {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(data, 'text/html');
+            var contenu = doc.querySelector('#conteneur_id').innerHTML;
+            document.getElementById('main').innerHTML = contenu;
+
+            
+        })
+        .catch(error => console.error(error));
+}
+
+
+document.addEventListener("chargerContenu", function () {
+    const loadContentButton = document.getElementById("loadContentButton");
+    const contentFromPage2 = document.getElementById("contentFromPage2");
+
+    loadContentButton.addEventListener("click", function () {
+        fetch("page2.html")
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(data, "text/html");
+                const conteneur2 = doc.getElementById("conteneur2");
+
+                contentFromPage2.innerHTML = conteneur2.innerHTML;
+
+                // Sélection de l'image de la page 2
+                const imageFromPage2 = contentFromPage2.querySelector("img");
+
+                // Modification de la classe CSS de l'image
+                imageFromPage2.classList.add("custom-image-style");
+            });
+    });
+});
+
+
+
+// signup
+
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+loginForm.style.marginLeft = "0%";
+loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+signupBtn.click();
+return false;
+});
